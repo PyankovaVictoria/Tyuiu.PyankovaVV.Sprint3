020f1109ed9d9258f0b9c9eb00d62b9c426fed7e
Tyuiu.PyankovaVV@outlook.com <vika.pyankova04@mail.ru>
2023-10-20 19:58:40 +0500

    Task1

---
+++ Tyuiu.PyankovaVV.Sprint3.Task1.V19.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PyankovaVV.Sprint3.Task1.V19.Lib;
+
+namespace Tyuiu.PyankovaVV.Sprint3.Task1.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void GetMultiplySeries()
+        {
+            DataService ds = new DataService();
+            int value = 1;
+            int startValue = 1;
+            int stopValue = 9;
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+            double wait = 96.895;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
