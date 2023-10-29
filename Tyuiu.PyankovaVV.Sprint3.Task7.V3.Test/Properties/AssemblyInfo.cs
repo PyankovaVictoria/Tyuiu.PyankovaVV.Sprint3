409db6e8cd1721c2dd4fbca4653f3a2d7aef93fc
Tyuiu.PyankovaVV@outlook.com <vika.pyankova04@mail.ru>
2023-10-29 23:05:57 +0500

    Task7

---
+++ Tyuiu.PyankovaVV.Sprint3.Task7.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PyankovaVV.Sprint3.Task7.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PyankovaVV.Sprint3.Task7.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d26a8af2-bcc5-42ed-a95c-2e8c69cbe1b1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PyankovaVV.Sprint3.Task7.V3/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PyankovaVV.Sprint3.Task7.V3.Lib;
+
+namespace Tyuiu.PyankovaVV.Sprint3.Task7.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнила: Пьянкова В.В. | АСОиУб-23-2";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнила: Пьянкова Виктория Вячеславовна | АСОиУб-23-2                  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Напишите программу, которая выводит таблицу значений функции, f(x) на   *");
+            Console.WriteLine("* заданном диапозоне [-5; 5]                                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*           3x - 1.5                                                      *");
+            Console.WriteLine("* F(x) = ---------------  + 2                                             *");
+            Console.WriteLine("*        sin(x) - 3 + x                                                   *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine(" Начало шага = " + startValue);
+            Console.WriteLine(" Конец шага  = " + stopValue);
+            Console.WriteLine("***************************************************************************");
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+            double[] valueArray;
+            valueArray = new double[len];
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("+----------+----------+");
+            Console.WriteLine("|    X     |    f(x)  |");
+            Console.WriteLine("+----------+----------+");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}     |  {1, 6:f2}  |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+----------+----------+");
+            Console.ReadKey();
+        }
+    }
+}
