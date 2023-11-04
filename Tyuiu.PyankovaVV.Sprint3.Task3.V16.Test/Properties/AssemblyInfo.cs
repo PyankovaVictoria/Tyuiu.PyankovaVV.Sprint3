8eefdc8fd38fb829c2dfab5458c2e8321fe77a9f
Tyuiu.PyankovaVV@outlook.com <vika.pyankova04@mail.ru>
2023-11-04 19:13:32 +0500

    Task3

---
+++ Tyuiu.PyankovaVV.Sprint3.Task3.V16.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PyankovaVV.Sprint3.Task3.V16.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PyankovaVV.Sprint3.Task3.V16.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("221d7677-2f5e-4a92-a3a2-7b78361c45b5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
